3a9f69c80212770bd57c87d611a4a8e82ef7d01b
jazmin <vjk0206@autuni.ac.nz>
2019-04-15 15:12:15 +1200

    home page

---
+++ app/src/main/java/com/example/autandroidapp/services_button.java
@@ -0,0 +1,20 @@
+package com.example.autandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+import com.example.autandroidapp.*;
+
+public class services_button extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services_button);
+
+        Intent intent = getIntent();
+    }
+}
