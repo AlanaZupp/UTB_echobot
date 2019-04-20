59b961cca327cb6739ffcae32b6d8149e0f010cd
rachel <songe993@gmail.com>
2019-04-20 18:25:20 +1200

    added map activity

---
+++ app/src/main/java/com/example/autandroidapp/CityFragment.java
@@ -0,0 +1,33 @@
+package com.example.autandroidapp;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CityFragment extends Fragment {
+
+
+    public CityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_city, container, false);
+    }
+
+}
--- app/src/main/java/com/example/autandroidapp/MainActivity.java
+++ app/src/main/java/com/example/autandroidapp/MainActivity.java
@@ -38,4 +38,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void MapActivity(View view)
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
 }
